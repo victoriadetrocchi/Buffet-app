61a61e29891e3370ddb1351e1b3fcf15e0fd185a
victoriadetrocchi <149382979+victoriadetrocchi@users.noreply.github.com>
2026-01-20 17:51:31 -0300

    Delete buffet-app directory

--- buffet-app/frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
-import { LoginComponent } from './features/auth/login/login.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent }
-];
-
