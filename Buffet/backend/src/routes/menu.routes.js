299d751e104d8ea32dcd35a5826d8c1438c1cd52
Victoria De Trocchi <149382979+victoriadetrocchi@users.noreply.github.com>
2026-02-11 23:19:43 -0300

    Delete Buffet directory

--- Buffet/backend/src/routes/menu.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const menuController = require('../controllers/menu.controller');
-
-router.get('/', menuController.listar);
-
-module.exports = router;
